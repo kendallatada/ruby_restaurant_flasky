24ae9eb1cf2323788b6d2f479dfb5bcfa9eaa951
Kendall Holmes <98422018+kendallatada@users.noreply.github.com>
2023-04-21 14:11:10 -0700

    4/21 livecode: get one resource, 400 and 404 errors

--- app/routes/restaurant.py
+++ app/routes/restaurant.py
@@ -30,3 +30,24 @@ def get_restaurants():
         response.append(restaurant_dict)
 
     return jsonify(response), 200
+
+#/restaurant/<id>
+@restaurant_bp.route("/<id>", methods=["GET"])
+def get_one_restaurant(id):
+    try:
+        restaurant_id = int(id)
+    except ValueError:
+        return {"message": f"invalid id: {id}"}, 400
+
+    for restaurant in restaurant_list:
+        if restaurant.id == restaurant_id:
+            return jsonify({
+                "id": restaurant.id,
+                "nested_example": {"hey": "hi"},
+                "rating": restaurant.rating,
+                "name": restaurant.name,
+                "cuisine": restaurant.cuisine,
+                "distance_from_ada": restaurant.distance_from_ada
+            }), 200
+        
+    return {"message": f"id {restaurant_id} not found"}, 404
