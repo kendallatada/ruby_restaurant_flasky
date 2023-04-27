13917c97af59392e4a8c7b8f7582a71c9817cddd
nancy-harris <nancy.harris@adadevelopersacademy.org>
2023-04-27 11:43:41 -0700

    Use new Restaurant model

--- app/models/restaurant.py
+++ app/models/restaurant.py
@@ -6,3 +6,12 @@ class Restaurant(db.Model):
     name = db.Column(db.String)
     cuisine = db.Column(db.String)
     distance_from_ada = db.Column(db.Integer)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "rating": self.rating,
+            "name": self.name,
+            "cuisine": self.cuisine,
+            "distance_from_ada": self.distance_from_ada
+        }
--- app/routes/restaurant.py
+++ app/routes/restaurant.py
@@ -1,52 +1,49 @@
-from flask import Blueprint, jsonify
-
-class Restaurant:
-    def __init__(self, id, rating, name, cuisine, distance_from_ada):
-        self.id = id
-        self.rating = rating
-        self.name = name
-        self.cuisine = cuisine
-        self.distance_from_ada = distance_from_ada
-
-restaurant1 = Restaurant(1, 5, "Pizza Hut", "Italian", 3.6)
-restaurant2 = Restaurant(15, 2, "Dominos", "American", 0.2)
-restaurant3 = Restaurant(9, 4.5, "Hong Kong Bistro", "Chinese", 0.1)
-
-restaurant_list = [restaurant1, restaurant2, restaurant3]
+from flask import Blueprint, jsonify, request
+from app import db
+from app.models.restaurant import Restaurant
 
 restaurant_bp = Blueprint("restaurant", __name__, url_prefix="/restaurant")
+
+@restaurant_bp.route("", methods=["POST"])
+def add_restaurant():
+    request_body = request.get_json()
+    new_restaurant = Restaurant(
+        rating = request_body["rating"],
+        name = request_body["name"],
+        cuisine = request_body["cuisine"],
+        distance_from_ada = request_body["distance_from_ada"]
+    )
+
+    db.session.add(new_restaurant)
+    db.session.commit()
+
+    return {"id": new_restaurant.id}, 201
 
 @restaurant_bp.route("", methods=["GET"])
 def get_restaurants():
     response = []
-    for restaurant in restaurant_list:
-        restaurant_dict = {
-            "id": restaurant.id,
-            "rating": restaurant.rating,
-            "name": restaurant.name,
-            "cuisine": restaurant.cuisine,
-            "distance_from_ada": restaurant.distance_from_ada
-        }
-        response.append(restaurant_dict)
+    all_restaurants = Restaurant.query.all()
+    for restaurant in all_restaurants:
+        response.append(restaurant.to_dict())
 
     return jsonify(response), 200
 
-#/restaurant/<id>
-@restaurant_bp.route("/<id>", methods=["GET"])
-def get_one_restaurant(id):
-    try:
-        restaurant_id = int(id)
-    except ValueError:
-        return {"message": f"invalid id: {id}"}, 400
+# #/restaurant/<id>
+# @restaurant_bp.route("/<id>", methods=["GET"])
+# def get_one_restaurant(id):
+#     try:
+#         restaurant_id = int(id)
+#     except ValueError:
+#         return {"message": f"invalid id: {id}"}, 400
 
-    for restaurant in restaurant_list:
-        if restaurant.id == restaurant_id:
-            return jsonify({
-                "id": restaurant.id,
-                "rating": restaurant.rating,
-                "name": restaurant.name,
-                "cuisine": restaurant.cuisine,
-                "distance_from_ada": restaurant.distance_from_ada
-            }), 200
+#     for restaurant in restaurant_list:
+#         if restaurant.id == restaurant_id:
+#             return jsonify({
+#                 "id": restaurant.id,
+#                 "rating": restaurant.rating,
+#                 "name": restaurant.name,
+#                 "cuisine": restaurant.cuisine,
+#                 "distance_from_ada": restaurant.distance_from_ada
+#             }), 200
         
-    return {"message": f"id {restaurant_id} not found"}, 404
+#     return {"message": f"id {restaurant_id} not found"}, 404
