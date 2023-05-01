85b10b3dc23cd10eedc6270f9238ef1d43545669
Kendall Holmes <98422018+kendallatada@users.noreply.github.com>
2023-05-01 13:12:27 -0700

    5/1 Livecode: Read from db, update, delete

--- app/routes/restaurant.py
+++ app/routes/restaurant.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, request
+from flask import abort, Blueprint, jsonify, make_response, request
 from app import db
 from app.models.restaurant import Restaurant
 
@@ -28,22 +28,49 @@ def get_restaurants():
 
     return jsonify(response), 200
 
-# #/restaurant/<id>
-# @restaurant_bp.route("/<id>", methods=["GET"])
-# def get_one_restaurant(id):
-#     try:
-#         restaurant_id = int(id)
-#     except ValueError:
-#         return {"message": f"invalid id: {id}"}, 400
+#/restaurant/<id>
+@restaurant_bp.route("/<rest_id>", methods=["GET"])
+def get_one_restaurant(rest_id):
+    restaurant = validate_restaurant(rest_id)
 
-#     for restaurant in restaurant_list:
-#         if restaurant.id == restaurant_id:
-#             return jsonify({
-#                 "id": restaurant.id,
-#                 "rating": restaurant.rating,
-#                 "name": restaurant.name,
-#                 "cuisine": restaurant.cuisine,
-#                 "distance_from_ada": restaurant.distance_from_ada
-#             }), 200
-        
-#     return {"message": f"id {restaurant_id} not found"}, 404
+    # if restaurant is None:
+    #     return {"message": f"id {restaurant_id} not found"}, 404
+    
+    return restaurant.to_dict(), 200
+
+
+@restaurant_bp.route("/<rest_id>", methods=["PUT"])
+def update_restaurant(rest_id):
+    restaurant = validate_restaurant(rest_id)
+
+    request_data = request.get_json()
+
+    restaurant.name = request_data["name"]
+    restaurant.cuisine = request_data["cuisine"]
+    restaurant.rating = request_data["rating"]
+    restaurant.distance_from_ada = request_data["distance_from_ada"]
+
+    db.session.commit()
+
+    return {"msg": f"restaurant {rest_id} successfully updated"}, 200
+
+
+@restaurant_bp.route("/<rest_id>", methods=["DELETE"])
+def delete_restaurant(rest_id):
+    restaurant = validate_restaurant(rest_id)
+
+    db.session.delete(restaurant)
+    db.session.commit()
+
+    return {"msg": f"restaurant {rest_id} successfully deleted"}, 200
+
+
+def validate_restaurant(rest_id):
+    try:
+        restaurant_id = int(rest_id)
+    except ValueError:
+        return abort(make_response({"message": f"invalid id: {rest_id}"}, 400))
+    
+    return Restaurant.query.get_or_404(restaurant_id)
+
+
